82c5bfc9b14a66ae24ebba1f37287023289b281e
ashraful-fahim <ashraful30june@gmail.com>
2025-07-21 16:06:46 +0600

    concept clearing of for loop

---
+++ addingConsec2.py
@@ -0,0 +1,11 @@
+result = 0
+
+for num in range(1,51):
+    result += num
+    print(num)
+
+print(result)
+
+
+for i in range(1, 20, 5):
+    print(i)
---
+++ addingconsec.py
@@ -0,0 +1,11 @@
+result = 0
+num = 1
+
+for _ in range(100):
+    result = result + num
+    num = num + 1
+
+print(result)
+
+#this is the program of adding 1+2+3+4+5+6....... upto range-1
+
