e4dccdd4d8f8259c0544e57e3ee3606a81dba8bf
ashraful-fahim <ashraful30june@gmail.com>
2025-07-21 15:05:38 +0600

    automated the turtle program in a for loop

--- kocchop.py
+++ kocchop.py
@@ -2,13 +2,21 @@ import turtle
 
 turtle.shape("turtle")
 
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
-turtle.forward(100)
-turtle.left(90)
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(100)
+# turtle.left(90)
+# turtle.forward(100)
+# turtle.left(90)
+
+# turtle.exitonclick()
+
+turtle.speed(2)
+
+for i in range(4):
+    turtle.forward(100)
+    turtle.left(90)
 
 turtle.exitonclick()
